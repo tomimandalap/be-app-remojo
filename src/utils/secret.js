8bf0c0323276212fb22214387ec34872b8ae86cb
tomimandalap <tommymandalaputra@gmail.com>
2024-01-27 10:25:50 +0700

    added env email and pass admin

--- src/utils/secret.js
+++ src/utils/secret.js
@@ -2,3 +2,5 @@ import "dotenv/config";
 
 export const PORT = process.env.PORT || 3001;
 export const URL_MONGODB = process.env.URL_MONGODB || "";
+export const EMAIL_ADMIN = process.env.EMAIL_ADMIN || "";
+export const PASS_ADMIN = process.env.PASS_ADMIN || "";
