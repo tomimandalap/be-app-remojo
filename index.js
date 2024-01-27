4fee526f0ad4336285ac26048ca1a6b71f7355b5
tomimandalap <tommymandalaputra@gmail.com>
2024-01-27 09:24:04 +0700

    setup cors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const PORT = 3001;
 const app = express();
+
+// DEFINE CORS
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send({
