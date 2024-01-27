a58442d26585b1d32e671c10e9061c6a778269a6
tomimandalap <tommymandalaputra@gmail.com>
2024-01-27 15:27:42 +0700

    added middleware upload img use multer

---
+++ src/middleware/uploadImg.js
@@ -0,0 +1,59 @@
+import multer from "multer";
+import path from "path";
+import message from "../utils/message.js";
+
+const Storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const Upload = multer({
+  storage: Storage,
+  limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname.toLowerCase());
+
+    const listAllowExt = [".jpg", ".jpeg", ".png"];
+
+    if (!listAllowExt.includes(ext)) {
+      cb(
+        {
+          name: "MulterError",
+          message: "Extention image must be jpg/jpeg/png",
+          code: "WRONG_EXT_FILE",
+          filed: file.fieldname,
+        },
+        false
+      );
+    }
+
+    cb(null, true);
+  },
+});
+
+export default function UploadImg(req, res, next) {
+  const upload = Upload.single("image");
+
+  upload(req, res, (error) => {
+    if (error) {
+      const { code, message: msg } = error;
+
+      const isLargeSizeFile = code === "LIMIT_FILE_SIZE";
+      const isWrongExtFile = code === "WRONG_EXT_FILE";
+
+      const codeRes = isLargeSizeFile ? 413 : isWrongExtFile ? 400 : 500;
+
+      return message(res, codeRes, "Error multer", {
+        errors: [
+          {
+            path: code,
+            message: msg,
+          },
+        ],
+      });
+    }
+
+    next();
+  });
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -13,6 +13,7 @@ import {
   validateUserID,
   customer,
 } from "../middleware/auth.js";
+import UploadImg from "../middleware/uploadImg.js";
 
 const userRoute = express.Router();
 
@@ -20,7 +21,14 @@ userRoute.post("/user/register", Register);
 userRoute.post("/user/login", Login);
 userRoute.get("/user", authentication, admin, List);
 userRoute.get("/user/:_id", authentication, Detail);
-userRoute.put("/user/:_id", authentication, customer, validateUserID, Updated);
+userRoute.put(
+  "/user/:_id",
+  authentication,
+  customer,
+  validateUserID,
+  UploadImg,
+  Updated
+);
 userRoute.patch("/user/restore/:_id", authentication, admin, Restored);
 userRoute.delete("/user/remove/:_id", authentication, admin, Removed);
 
