e52740e4b0bad35929c464865400a15016a1304a
tomimandalap <tommymandalaputra@gmail.com>
2024-01-27 13:42:06 +0700

    added api detail user

---
+++ src/controllers/user/detail.get.js
@@ -0,0 +1,72 @@
+import userModel from "../../models/users.js";
+import message from "../../utils/message.js";
+import { Types } from "mongoose";
+
+/**
+ * @typedef {import('express').Request} ExpressRequest
+ * @typedef {import('express').Response} ExpressResponse
+ */
+
+/**
+ * @param {ExpressRequest} req
+ * @param {ExpressResponse} res
+ */
+
+export default async function (req, res) {
+  try {
+    const _id = req.params._id;
+
+    const data = await userModel.aggregate([
+      {
+        $lookup: {
+          from: "roles",
+          foreignField: "_id",
+          localField: "role_id",
+          as: "detail_role",
+        },
+      },
+      {
+        $unwind: "$detail_role",
+      },
+      {
+        $lookup: {
+          from: "storages",
+          foreignField: "_id",
+          localField: "storage_id",
+          as: "detail_storage",
+        },
+      },
+      {
+        $unwind: {
+          path: "$detail_storage",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $addFields: {
+          detail_storage: {
+            $ifNull: ["$detail_storage", null],
+          },
+        },
+      },
+      {
+        $project: {
+          password: 0,
+        },
+      },
+      {
+        $match: {
+          _id: new Types.ObjectId(_id),
+          "detail_role.name": "customer",
+          deleted_at: null,
+        },
+      },
+    ]);
+
+    if (!data.length) return message(res, 404, "Data user not found");
+
+    message(res, 200, "Detail user", data[0]);
+  } catch (error) {
+    message(res, 500, error?.message || "Internal server error");
+  }
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,13 @@ import express from "express";
 import registerPost from "../controllers/user/register.post.js";
 import loginPost from "../controllers/user/login.post.js";
 import listGet from "../controllers/user/list.get.js";
+import detailGet from "../controllers/user/detail.get.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/user/register", registerPost);
 userRoute.post("/user/login", loginPost);
 userRoute.get("/user", listGet);
+userRoute.get("/user/:_id", detailGet);
 
 export default userRoute;
