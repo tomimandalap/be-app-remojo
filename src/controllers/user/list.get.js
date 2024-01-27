a31b4c288e0d36cc15d67af5ab4a92b60dcd5556
tomimandalap <tommymandalaputra@gmail.com>
2024-01-27 13:32:44 +0700

    added api list user

---
+++ src/controllers/user/list.get.js
@@ -0,0 +1,99 @@
+import userModel from "../../models/users.js";
+import message from "../../utils/message.js";
+
+/**
+ * @typedef {import('express').Request} ExpressRequest
+ * @typedef {import('express').Response} ExpressResponse
+ */
+
+/**
+ * @param {ExpressRequest} req
+ * @param {ExpressResponse} res
+ */
+
+export default async function (req, res) {
+  try {
+    const q = req.query.q || "";
+    const sort_by = req.query.sort_by ? req.query.sort_by : "desc"; // mongoose 1 = asc or -1 desc
+    const page = req.query.page ? Number(req.query.page) : 1;
+    const per_page = req.query.per_page ? Number(req.query.per_page) : 10;
+
+    const skip = page > 1 ? (page - 1) * per_page : 0;
+
+    const filters = [
+      {
+        $lookup: {
+          from: "roles",
+          foreignField: "_id",
+          localField: "role_id",
+          as: "detail_role",
+        },
+      },
+      {
+        $unwind: "$detail_role",
+      },
+      {
+        $lookup: {
+          from: "storages",
+          foreignField: "_id",
+          localField: "storage_id",
+          as: "detail_storage",
+        },
+      },
+      {
+        $unwind: {
+          path: "$detail_storage",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $addFields: {
+          detail_storage: {
+            $ifNull: ["$detail_storage", null],
+          },
+        },
+      },
+      {
+        $project: {
+          password: 0,
+        },
+      },
+    ];
+
+    const data = await userModel
+      .aggregate([
+        ...filters,
+        {
+          $match: {
+            $or: [
+              { first_name: { $regex: q, $options: "i" } },
+              { last_name: { $regex: q, $options: "i" } },
+              { email: { $regex: q, $options: "i" } },
+              { phone: { $regex: q, $options: "i" } },
+            ],
+            "detail_role.name": "customer",
+            deleted_at: null,
+          },
+        },
+      ])
+      .sort({ _id: sort_by }) // mongoose 1 = asc or -1 desc
+      .skip(skip)
+      .limit(per_page);
+
+    const countDocuments = await userModel
+      .aggregate([
+        ...filters,
+        { $match: { "detail_role.name": "customer", deleted_at: null } },
+      ])
+      .count("total");
+    const pagination = {
+      page,
+      per_page,
+      total: countDocuments.length ? countDocuments[0].total : 0,
+    };
+
+    message(res, 200, "Data user", data, pagination);
+  } catch (error) {
+    message(res, 500, error?.message || "Internal server error");
+  }
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,12 @@
 import express from "express";
 import registerPost from "../controllers/user/register.post.js";
 import loginPost from "../controllers/user/login.post.js";
+import listGet from "../controllers/user/list.get.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/user/register", registerPost);
 userRoute.post("/user/login", loginPost);
+userRoute.get("/user", listGet);
 
 export default userRoute;
