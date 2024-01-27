463632a400912829d79ca38d3410516db158e4a0
tomimandalap <tommymandalaputra@gmail.com>
2024-01-27 11:51:17 +0700

    added env secret_key

--- src/utils/secret.js
+++ src/utils/secret.js
@@ -4,3 +4,4 @@ export const PORT = process.env.PORT || 3001;
 export const URL_MONGODB = process.env.URL_MONGODB || "";
 export const EMAIL_ADMIN = process.env.EMAIL_ADMIN || "";
 export const PASS_ADMIN = process.env.PASS_ADMIN || "";
+export const SECRET_KEY = process.env.SECRET_KEY;
