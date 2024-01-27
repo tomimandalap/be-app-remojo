223c399dd62be50c004eadbb3fc227f50c8a80a3
tomimandalap <tommymandalaputra@gmail.com>
2024-01-27 10:10:27 +0700

    added userModel

---
+++ src/models/users.js
@@ -0,0 +1,51 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role_id: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Roles", // relation collection users to roles
+    },
+    storage_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Storages", // relation collection users to storages
+      default: null,
+    },
+    deleted_at: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+const userModel = model("Users", userSchema);
+
+export default userModel;
