f541ae18a01a3ad65dd016d49f8812816d9ce683
tomimandalap <tommymandalaputra@gmail.com>
2024-01-28 11:42:18 +0700

    added api update product admin

---
+++ src/controllers/product/update.put.js
@@ -0,0 +1,100 @@
+import productModel from "../../models/product.js";
+import storageModel from "../../models/storage.js";
+import message from "../../utils/message.js";
+import validate from "../../utils/validate.js";
+import cloudinary from "cloudinary";
+
+import { z } from "zod";
+
+const schemaValidation = z.object({
+  name: z
+    .string()
+    .min(1, "Name is required")
+    .max(50, 'Name must not exceed 50 characters"')
+    .trim(),
+  price: z.number().int().positive(),
+  description: z
+    .string()
+    .min(10, "Description must have at least 10 letters")
+    .max(350, "Description must not exceed 350 characters")
+    .trim(),
+});
+
+/**
+ * @typedef {import('express').Request} ExpressRequest
+ * @typedef {import('express').Response} ExpressResponse
+ */
+
+/**
+ * @param {ExpressRequest} req
+ * @param {ExpressResponse} res
+ */
+
+export default async function (req, res) {
+  const file = req.file;
+  try {
+    // FIND DATA PRODUCT BY ID
+    const _id = req.params._id;
+    const findProductByID = await productModel.findOne({
+      _id,
+      deleted_at: null,
+    });
+
+    if (!findProductByID) return message(res, 404, "Product not found");
+
+    const body = req.body;
+    const checkValidate = validate(schemaValidation, {
+      ...body,
+      price: Number(body.price),
+    });
+
+    // CHECK VALIDATION FORM BODY
+    if (!checkValidate.success)
+      return message(res, 422, "Error validation", {
+        errors: checkValidate.errors,
+      });
+
+    let payload = {};
+    if (file) {
+      // FIND DATA IMG ON STORAGE BY STORAGE_ID
+      const storage_id = findProductByID._doc.storage_id;
+      const findImgByStorageID = await storageModel.findById({
+        _id: storage_id,
+      });
+
+      // DELETE IMG ON CLOUDINARY AND STORAGE
+      await cloudinary.v2.uploader.destroy(findImgByStorageID._doc.public_id);
+      await storageModel.deleteOne({ _id: storage_id });
+
+      // UPLOAD IMG ON CLOUDINARY
+      const uploadResult = await new Promise((resolve) => {
+        cloudinary.v2.uploader
+          .upload_stream((error, uploadResult) => {
+            return resolve(uploadResult);
+          })
+          .end(file.buffer);
+      });
+
+      // CREATE DOCUMENT ON STORAGE
+      const createStorege = await storageModel.create({
+        public_id: uploadResult.public_id,
+        secure_url: uploadResult.secure_url,
+      });
+
+      // ASSIGNT STORAGE_ID FORM RESPONSE CREATE STORAGE
+      payload.storage_id = createStorege._doc._id;
+    }
+
+    const updateProduct = await productModel.findByIdAndUpdate(
+      { _id },
+      { ...payload, ...checkValidate.data },
+      { new: true }
+    );
+
+    message(res, 200, "Update product success", updateProduct);
+  } catch (error) {
+    message(res, 500, error?.message || "Internal server error");
+  } finally {
+    if (file && file.buffer) file.buffer = undefined;
+  }
+}
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,6 +2,7 @@ import express from "express";
 import Create from "../controllers/product/create.post.js";
 import All from "../controllers/product/all.get.js";
 import Detail from "../controllers/product/detail.get.js";
+import Update from "../controllers/product/update.put.js";
 import Restore from "../controllers/product/restore.patch.js";
 import Remove from "../controllers/product/remove.delete.js";
 
@@ -13,6 +14,7 @@ const productRoute = express.Router();
 productRoute.post("/product/new", authentication, admin, UploadImg, Create);
 productRoute.get("/product", authentication, admin, All);
 productRoute.get("/product/:_id", authentication, admin, Detail);
+productRoute.put("/product/:_id", authentication, admin, UploadImg, Update);
 productRoute.patch("/product/restore/:_id", authentication, admin, Restore);
 productRoute.delete("/product/remove/:_id", authentication, admin, Remove);
 
