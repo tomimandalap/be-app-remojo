d12231325e9d3a131c7f8da8de47bf36c7bdbb51
tomimandalap <tommymandalaputra@gmail.com>
2024-01-28 11:01:02 +0700

    added api remove product admin

---
+++ src/controllers/product/remove.delete.js
@@ -0,0 +1,30 @@
+import productModel from "../../models/product.js";
+import message from "../../utils/message.js";
+
+/**
+ * @typedef {import('express').Request} ExpressRequest
+ * @typedef {import('express').Response} ExpressResponse
+ */
+
+/**
+ * @param {ExpressRequest} req
+ * @param {ExpressResponse} res
+ */
+
+export default async function (req, res) {
+  try {
+    const _id = req.params._id;
+
+    const findProductByID = await productModel.findOneAndUpdate(
+      { _id, deleted_at: null },
+      { deleted_at: new Date() },
+      { new: true }
+    );
+
+    if (!findProductByID) return message(res, 404, "Product not found");
+
+    message(res, 200, "Remove product success");
+  } catch (error) {
+    message(res, 500, error?.message || "Internal server error");
+  }
+}
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,6 +2,7 @@ import express from "express";
 import Create from "../controllers/product/create.post.js";
 import All from "../controllers/product/all.get.js";
 import Detail from "../controllers/product/detail.get.js";
+import Remove from "../controllers/product/remove.delete.js";
 
 import UploadImg from "../middleware/uploadImg.js";
 import { authentication, admin } from "../middleware/auth.js";
@@ -10,6 +11,7 @@ const productRoute = express.Router();
 
 productRoute.post("/product/new", authentication, admin, UploadImg, Create);
 productRoute.get("/product", authentication, admin, All);
-productRoute.get("/product/:_id", authentication, Detail);
+productRoute.get("/product/:_id", authentication, admin, Detail);
+productRoute.delete("/product/remove/:_id", authentication, admin, Remove);
 
 export default productRoute;
