4e5bd93493de42a18c3e56bb25d6615dea6ece2a
tomimandalap <tommymandalaputra@gmail.com>
2024-01-28 09:42:10 +0700

    added schema model products

---
+++ src/models/product.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    storage_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Storages",
+      required: true,
+    },
+    deleted_at: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+const productModel = model("Products", productSchema);
+
+export default productModel;
