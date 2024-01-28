19b99fff49ad0bf859d59a5ef37930ce93e07ca3
tomimandalap <tommymandalaputra@gmail.com>
2024-01-28 10:50:40 +0700

    added api detail product admin

---
+++ src/controllers/product/detail.get.js
@@ -0,0 +1,44 @@
+import productModel from "../../models/product.js";
+import message from "../../utils/message.js";
+import { Types } from "mongoose";
+
+/**
+ * @typedef {import('express').Request} ExpressRequest
+ * @typedef {import('express').Response} ExpressResponse
+ */
+
+/**
+ * @param {ExpressRequest} req
+ * @param {ExpressResponse} res
+ */
+
+export default async function (req, res) {
+  try {
+    const _id = req.params._id;
+
+    const detail = await productModel.aggregate([
+      {
+        $match: {
+          _id: new Types.ObjectId(_id),
+        },
+      },
+      {
+        $lookup: {
+          from: "storages",
+          foreignField: "_id",
+          localField: "storage_id",
+          as: "detail_storage",
+        },
+      },
+      {
+        $unwind: "$detail_storage",
+      },
+    ]);
+
+    if (!detail.length) return message(res, 404, "Product not found");
+
+    message(res, 200, "Detail product", detail[0]);
+  } catch (error) {
+    message(res, 500, error?.message || "Internal server error");
+  }
+}
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,13 +1,15 @@
 import express from "express";
 import Create from "../controllers/product/create.post.js";
 import All from "../controllers/product/all.get.js";
-import UploadImg from "../middleware/uploadImg.js";
+import Detail from "../controllers/product/detail.get.js";
 
+import UploadImg from "../middleware/uploadImg.js";
 import { authentication, admin } from "../middleware/auth.js";
 
 const productRoute = express.Router();
 
 productRoute.post("/product/new", authentication, admin, UploadImg, Create);
 productRoute.get("/product", authentication, admin, All);
+productRoute.get("/product/:_id", authentication, Detail);
 
 export default productRoute;
