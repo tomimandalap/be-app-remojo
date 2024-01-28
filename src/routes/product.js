395807b5e495d6f728b6d502351ac4cff0228e00
tomimandalap <tommymandalaputra@gmail.com>
2024-01-28 11:04:14 +0700

    added api restore product admin

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,6 +2,7 @@ import express from "express";
 import Create from "../controllers/product/create.post.js";
 import All from "../controllers/product/all.get.js";
 import Detail from "../controllers/product/detail.get.js";
+import Restore from "../controllers/product/restore.patch.js";
 import Remove from "../controllers/product/remove.delete.js";
 
 import UploadImg from "../middleware/uploadImg.js";
@@ -12,6 +13,7 @@ const productRoute = express.Router();
 productRoute.post("/product/new", authentication, admin, UploadImg, Create);
 productRoute.get("/product", authentication, admin, All);
 productRoute.get("/product/:_id", authentication, admin, Detail);
+productRoute.patch("/product/restore/:_id", authentication, admin, Restore);
 productRoute.delete("/product/remove/:_id", authentication, admin, Remove);
 
 export default productRoute;
