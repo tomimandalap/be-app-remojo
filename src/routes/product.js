905e5d6c02acd53479d3608b25a6643b0cd8187c
tomimandalap <tommymandalaputra@gmail.com>
2024-01-28 10:43:24 +0700

    added api all data product

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,13 @@
 import express from "express";
 import Create from "../controllers/product/create.post.js";
-import { authentication, admin } from "../middleware/auth.js";
+import All from "../controllers/product/all.get.js";
 import UploadImg from "../middleware/uploadImg.js";
+
+import { authentication, admin } from "../middleware/auth.js";
 
 const productRoute = express.Router();
 
 productRoute.post("/product/new", authentication, admin, UploadImg, Create);
+productRoute.get("/product", authentication, admin, All);
 
 export default productRoute;
