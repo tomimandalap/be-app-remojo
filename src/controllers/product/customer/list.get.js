70228c58c3477b32f11f77dc48180dcda5407f7a
tomimandalap <tommymandalaputra@gmail.com>
2024-02-03 12:26:18 +0700

    update filter by start_Date and end_date customer product

--- src/controllers/product/customer/list.get.js
+++ src/controllers/product/customer/list.get.js
@@ -1,5 +1,18 @@
 import productModel from "../../../models/product.js";
+import transactionModel from "../../../models/transaction.js";
 import message from "../../../utils/message.js";
+
+import validate from "../../../utils/validate.js";
+import { z } from "zod";
+
+const schemaValidation = z
+  .object({
+    start_date: z.coerce.date(),
+    end_date: z.coerce.date(),
+  })
+  .refine((data) => {
+    return !data.end_date || data.start_date <= data.end_date;
+  }, "start_date must not be greater than end_date");
 
 /**
  * @typedef {import('express').Request} ExpressRequest
@@ -13,6 +26,32 @@ import message from "../../../utils/message.js";
 
 export default async function (req, res) {
   try {
+    const start_date = req.query.start_date;
+    const end_date = req.query.end_date;
+
+    // CHECK VALIDATION FORM QUERY
+    const checkValidate = validate(schemaValidation, req.query);
+
+    if (!checkValidate.success)
+      return message(res, 422, "Error validation", {
+        errors: checkValidate.errors,
+      });
+
+    const findTransaction = await transactionModel.find({
+      $or: [
+        {
+          "rental_duration.start_date": { $lte: new Date(end_date) },
+          "rental_duration.end_date": { $gte: new Date(start_date) },
+        },
+      ],
+      status: { $nin: ["failure", "refund"] },
+      deleted_at: null,
+    });
+
+    let product_ids = findTransaction
+      .map((transaction) => transaction._doc.product_ids)
+      .flat();
+
     const q = req.query.q || "";
     const page = req.query.page ? Number(req.query.page) : 1;
     const per_page = req.query.per_page ? Number(req.query.per_page) : 10;
@@ -23,6 +62,8 @@ export default async function (req, res) {
       {
         $match: {
           name: { $regex: q, $options: "i" },
+          _id: { $nin: product_ids },
+          deleted_at: null,
         },
       },
       {
