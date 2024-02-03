ea9d41a90a99046fa0308e0d9bc13be288397b66
tomimandalap <tommymandalaputra@gmail.com>
2024-02-03 09:50:32 +0700

    added api detial product customer

--- src/routes/product.js
+++ src/routes/product.js
@@ -5,10 +5,12 @@ import Detail from "../controllers/product/detail.get.js";
 import Update from "../controllers/product/update.put.js";
 import Restore from "../controllers/product/restore.patch.js";
 import Remove from "../controllers/product/remove.delete.js";
+
 import List from "../controllers/product/customer/list.get.js";
+import ProductDetail from "../controllers/product/customer/detail.get.js";
 
 import UploadImg from "../middleware/uploadImg.js";
-import { authentication, admin } from "../middleware/auth.js";
+import { authentication, admin, customer } from "../middleware/auth.js";
 
 const productRoute = express.Router();
 
@@ -20,5 +22,11 @@ productRoute.patch("/product/restore/:_id", authentication, admin, Restore);
 productRoute.delete("/product/remove/:_id", authentication, admin, Remove);
 
 productRoute.get("/customer/product", List); // customer
+productRoute.get(
+  "/customer/product/:_id",
+  authentication,
+  customer,
+  ProductDetail
+); // customer
 
 export default productRoute;
