bd15caf62bf3773f1d88e662b6f81dd24ec8f4e6
tomimandalap <tommymandalaputra@gmail.com>
2024-06-08 17:58:08 +0700

    feat: added autoIndex

--- src/config/connection.js
+++ src/config/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { URL_MONGODB } from "../utils/secret.js";
 
 try {
-  await mongoose.connect(URL_MONGODB);
+  await mongoose.connect(URL_MONGODB, { autoIndex: true, });
   console.log("DB connection success");
 } catch (error) {
   console.error.error("DB connection failed", error);
